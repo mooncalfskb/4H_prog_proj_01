8028c7ead07b7a1d05e46b961f089e5b8442d527
Sherrod Blankner <mooncalf@Mooncalf.local>
2018-10-10 22:19:52 -0700

    got form working with bootstrap

---
+++ js/fourH_IBM_tables.js
@@ -0,0 +1,6 @@
+/*!
+IBM TABLE JSON, written by Sherrod Blankner, Phlebas Consulting
+
+ */
+
+var IBM_json = [{"ASCII":"A","Decimal":"65","Hex":"41","Octal":"101","Binary":"1000001"},{"ASCII":"B","Decimal":"66","Hex":"42","Octal":"102","Binary":"1000010"}];
---
+++ js/fourH_doc_ready.js
@@ -0,0 +1,13 @@
+/*!
+4H Doc Ready Class 01, written by Sherrod Blankner, Phlebas Consulting
+
+ */
+
+    //set up interactions
+    $( document ).ready(function() { 	
+    
+    	$( "#binary_submit" ).click(function() {
+    		 alert(IBM_json);
+		});	
+    
+    });
